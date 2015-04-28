4f738cd43bc7f587fff424816b05425ff451f68b
Scott Abernethy <scott@pixietwin.com>
2015-04-28 21:42:59 +1200

    Details

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+# Add command if needed
 if [ "${1:0:1}" = '-' ]; then
 	set -- mongod "$@"
 fi
 
+# Drop root privileges if we are running the service
 if [ "$1" = 'mongod' ]; then
 	chown -R mongodb /data/db
 
@@ -16,4 +18,5 @@ if [ "$1" = 'mongod' ]; then
 	exec gosu mongodb "$@"
 fi
 
+# Assume user wanting to run own process e.g. `bash` to explore this image.
 exec "$@"
